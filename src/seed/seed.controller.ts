2223f25c587015f7f94b74faa484fa68f23461f5
FacundoCastillo12 <facuelfacha15@gmail.com>
2023-04-12 21:12:10 -0300

    Antes de insertar por lote

---
+++ src/seed/seed.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import axios from 'axios';
+import { SeedService } from './seed.service';
+
+@Controller('seed')
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+
+  @Get()
+  executeSeed() {
+    return this.seedService.executeSeed();
+  }
+}
